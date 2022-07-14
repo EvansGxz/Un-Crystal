2437563e1afd1bc42b5f7c1343597390adff7127
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-07-14 16:15:54 -0500

    Testing aside bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import CardPublish from "./Components/Card/card";
-import User from "./assets/user_me.png"
-import Verify from "./assets/verify/verify.png"
+
+import { Header } from "./Components/Structure/header";
+import Main from "./Components/Structure/main";
 function App() {
   return (
-    <CardPublish
-      id= "1"
-      name="Evans Glz"
-      src={User}
-      onClick={()=>console.log("hola")}
-      icon={Verify}
-      userId="@EvansGxz"
-      body="Deserunt esse cillum anim proident qui. Ipsum ut minim quis irure est sunt velit ipsum nostrud non commodo ut ipsum nulla. Pariatur tempor reprehenderit sunt reprehenderit veniam."
-    />
-  );
+    <>
+    <Header/>
+    <Main/>
+  </>);
 }
 
 export default App;
