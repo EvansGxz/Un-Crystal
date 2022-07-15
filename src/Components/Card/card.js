8db53a87339a991cc1942bd25bf1b0169b962afe
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-07-15 14:58:54 -0500

    remove shadow

--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -15,7 +15,7 @@ const Container = styled.div`
   background-color: ${colors.black};
   width: 50vw;
   border-radius: 20px;
-  box-shadow: 0px 20px 20px rgba(57, 57, 57, 0.1);
+  
   margin: 1rem;
   padding: 0.225rem;
   
